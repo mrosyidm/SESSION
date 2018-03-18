a1f4739bbfc105e36c978d5ff1ebb101250442da
mrosyidm <37059186+mrosyidm@users.noreply.github.com>
2018-03-18 21:33:24 +0700

    Update session.php

--- session.php
+++ session.php
@@ -7,13 +7,45 @@
 		<br><br>
 		Selamat Datang 
 		<?php 
-		echo $_POST['usr'];
-		session_start();
-		    if ( !isset( $_SESSION['hitung'] ) ) 
-		     $_SESSION['hitung'] = 1; 
-		 else $_SESSION['hitung']++;
-		   echo "<br> Anda telah mengakses halaman ini sebanyak ".$_SESSION['hitung']." kali pada session ini" ; 
-		 ?>
+		<h2>
+<?php 
+session_start();
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'Netscape')){
+    $browser = 'Netscape';
+} else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox')){
+    $browser = 'Firefox';
+} else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome')){
+    $browser = 'Chrome';
+} else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera')){
+    $browser = 'Opera';
+} else if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE')){
+    $browser = 'Internet Explorer';
+} else $browser = 'Other';
+$_SESSION['web'] = $browser; 
+
+date_default_timezone_set("Asia/Jakarta");
+
+$b = time();
+$hour = date("G",$b);
+
+if ($hour>=0 && $hour<=11)
+{
+echo " Selamat Pagi :)";
+}
+elseif ($hour >=12 && $hour<=14)
+{
+echo " Selamat Siang :) ";
+}
+elseif ($hour >=15 && $hour<=17)
+{
+echo " Selamat Sore :) ";
+}
+elseif ($hour >=17 && $hour<=23)
+{
+echo " Selamat Malam :) ";
+}
+echo " Browser Anda adalah ".$_SESSION['web'];
+ ?> 
 		 </h2>
 </body>
 </html>
